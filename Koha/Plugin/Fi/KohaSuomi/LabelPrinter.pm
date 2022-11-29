9cc86beb7e4520bc9c539309a0fe8c279d8abd26
Lari Taskula <lari.taskula@hypernova.fi>
2022-11-29 00:05:19 +0000

    Release 1.0.8

--- Koha/Plugin/Fi/KohaSuomi/LabelPrinter.pm
+++ Koha/Plugin/Fi/KohaSuomi/LabelPrinter.pm
@@ -28,7 +28,7 @@ use Koha::Plugin::Fi::KohaSuomi::LabelPrinter::PdfCreator;
 
 use Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::Labels::UnknownItems;
 
-our $VERSION = "1.0.7";
+our $VERSION = "1.0.8";
 our $MINIMUM_VERSION = "22.05.00.000";
 
 ## Here is our metadata, some keys are required, some are optional
@@ -36,7 +36,7 @@ our $metadata = {
     name            => 'Koha-Suomi Label Printer',
     author          => 'Olli-Antti Kivilahti',
     date_authored   => '2014-01-30',
-    date_updated    => "2022-11-10",
+    date_updated    => "2022-11-29",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,

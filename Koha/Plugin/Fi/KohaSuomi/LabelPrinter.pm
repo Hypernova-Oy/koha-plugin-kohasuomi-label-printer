2adafa10077d9d76a505cc1837567ffb3848b836
Olli-Antti Kivilahti <Olli-Antti.Kivilahti@Hypernova.fi>
2023-04-25 15:06:08 +0300

    Release version 1.0.12

--- Koha/Plugin/Fi/KohaSuomi/LabelPrinter.pm
+++ Koha/Plugin/Fi/KohaSuomi/LabelPrinter.pm
@@ -28,7 +28,7 @@ use Koha::Plugin::Fi::KohaSuomi::LabelPrinter::PdfCreator;
 
 use Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::Labels::UnknownItems;
 
-our $VERSION = "1.0.11";
+our $VERSION = "1.0.12";
 our $MINIMUM_VERSION = "22.05.00.000";
 
 ## Here is our metadata, some keys are required, some are optional
@@ -36,7 +36,7 @@ our $metadata = {
     name            => 'Koha-Suomi Label Printer',
     author          => 'Olli-Antti Kivilahti',
     date_authored   => '2014-01-30',
-    date_updated    => "2023-04-20",
+    date_updated    => "2023-04-25",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,

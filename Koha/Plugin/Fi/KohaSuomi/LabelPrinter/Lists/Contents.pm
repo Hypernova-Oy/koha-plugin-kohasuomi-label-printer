9f794cc6bbe18283159bd1bfd84ba0039bb70584
Olli-Antti Kivilahti <Olli-Antti.Kivilahti@Hypernova.fi>
2024-01-09 22:30:05 +0200

    Cater to Bug 33379: Remove virtualshelfcontents.flags
    Removing the flags-column crashes the plugin. Migrate to a plugin-specific item print list table.

--- Koha/Plugin/Fi/KohaSuomi/LabelPrinter/Lists/Contents.pm
+++ Koha/Plugin/Fi/KohaSuomi/LabelPrinter/Lists/Contents.pm
@@ -19,24 +19,7 @@ sub add {
         }
 
         if ($body->{listname} eq 'labels printing') {
-            my $shelf = Koha::Virtualshelves->find( { owner => $user->borrowernumber, shelfname => $body->{listname} } );
-            if (!$shelf) {
-                $shelf = eval { Koha::Virtualshelf->new( {
-                    shelfname => 'labels printing',
-                    owner => $user->borrowernumber,
-                    sortfield => undef,
-                    allow_change_from_owner => 1,
-                    allow_change_from_others => 0,
-                    } )->store; };
-            }
-            my $content = Koha::Virtualshelfcontent->new(
-                {
-                    shelfnumber => $shelf->shelfnumber,
-                    biblionumber => $body->{biblionumber},
-                    borrowernumber => $body->{borrowernumber},
-                    flags => $body->{itemnumber},
-                }
-            )->store;
+            my $content = addDB($body->{borrowernumber}, $body->{itemnumber});
             return $c->render(status => 200, openapi => $content);
         } else {
             return $c->render( status => 400, openapi => {});
@@ -49,21 +32,94 @@ sub delete {
     my $c = shift->openapi->valid_input or return;
 
     my $body = $c->req->json;
-    my $shelf = Koha::Virtualshelves->find({owner => $body->{borrowernumber}, shelfname => $body->{listname}});
-    my $content = $shelf->get_contents;
-    unless ($content) {
-        return $c->render( status  => 404,
-                           openapi => { error => "Notice not found" } );
-    }
+    my $res = deleteDB($body->{borrowernumber});
 
-    my $res = $content->delete;
-
-    if ($res eq '1') {
+    if ($res >= 0) {
         return $c->render( status => 200, openapi => {});
-    } elsif ($res eq '-1') {
+    } elsif ($res == 0) {
         return $c->render( status => 404, openapi => {});
     } else {
         return $c->render( status => 400, openapi => {});
+    }
+}
+
+sub getDB {
+    my ($id) = @_;
+
+    my $dbh = C4::Context->dbh();
+    my $label_table = Koha::Plugin::Fi::KohaSuomi::LabelPrinter->new->get_qualified_table_name('label_print_list');
+
+    my @params;
+    my $sql = "SELECT * FROM $label_table WHERE id = ?";
+
+    my $sth = $dbh->prepare($sql);
+    eval {
+        $sth->execute( $id );
+    };
+    if ($@ || $sth->err) {
+        my @cal = caller(0);
+        Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::DB->throw(error => $cal[3].'():>'.($@ || $sth->errstr));
+    }
+    return $sth->fetchrow_hashref();
+}
+
+sub listDB {
+    my ($borrowernumber) = @_;
+
+    my $dbh = C4::Context->dbh();
+    my $label_table = Koha::Plugin::Fi::KohaSuomi::LabelPrinter->new->get_qualified_table_name('label_print_list');
+
+    my @params;
+    my $sql = "SELECT * FROM $label_table WHERE borrowernumber = ?";
+
+    my $sth = $dbh->prepare($sql);
+    eval {
+        $sth->execute( $borrowernumber );
+    };
+    if ($@ || $sth->err) {
+        my @cal = caller(0);
+        Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::DB->throw(error => $cal[3].'():>'.($@ || $sth->errstr));
+    }
+    my @rows = $sth->fetchrow_hashref();
+    return \@rows;
+}
+
+sub addDB {
+    my ($borrowernumber, $itemnumber) = @_;
+
+    my $dbh = C4::Context->dbh();
+    my $label_table = Koha::Plugin::Fi::KohaSuomi::LabelPrinter->new->get_qualified_table_name('label_print_list');
+
+    my @params;
+    my $sql = "INSERT INTO $label_table (borrowernumber, itemnumber) VALUES (?, ?)";
+
+    my $sth = $dbh->prepare($sql);
+    eval {
+        $sth->execute( $borrowernumber, $itemnumber );
+    };
+    if ($@ || $sth->err) {
+        my @cal = caller(0);
+        Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::DB->throw(error => $cal[3].'():>'.($@ || $sth->errstr));
+    }
+    return getDB($sth->{mysql_insertid});
+}
+
+sub deleteDB {
+    my ($borrowernumber) = @_;
+
+    my $dbh = C4::Context->dbh();
+    my $label_table = Koha::Plugin::Fi::KohaSuomi::LabelPrinter->new->get_qualified_table_name('label_print_list');
+
+    my @params;
+    my $sql = "DELETE FROM $label_table WHERE borrowernumber = ?";
+
+    my $sth = $dbh->prepare($sql);
+    eval {
+        return $sth->execute( $borrowernumber );
+    };
+    if ($@ || $sth->err) {
+        my @cal = caller(0);
+        Koha::Plugin::Fi::KohaSuomi::LabelPrinter::Exceptions::DB->throw(error => $cal[3].'():>'.($@ || $sth->errstr));
     }
 }
 
